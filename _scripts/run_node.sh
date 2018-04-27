3302ab578c65863e5574573a0ee3490f2bbea591
Ardhi Putra Pratama H <me@ardhipoetra.web.id>
2018-04-27 11:41:03 +0700

    modifying script to handle DMCK parameters

--- _scripts/run_node.sh
+++ _scripts/run_node.sh
@@ -1,19 +1,41 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# how to run : run_node <node_id> <logging directory> <[mine/nomine]> <DMCK ipc directory>
+echo "Running the run_node.sh"
+curdate=`date +"%Y%m%dT%H%M"`
 
 node_id=$1
-opt=''
+log_dir=$ETH_PATH/node$node_id/
+ipc_dir=""
+
+opt=" --selfId=$node_id"
+
 if [ -n "$2" ]; then
   if [ "$2" == "mine" ]; then
-    opt='--mine --minerthreads=1'
+    opt="$opt --mine --minerthreads=1"
+  else
+    log_dir=$2
   fi
 fi
 
-curdate=`date +"%Y%m%dT%H%M"`
+if [ -n "$3" ]; then
+  if [ "$3" == "mine" ]; then
+    opt="$opt --mine --minerthreads=1"
+  fi
+fi
 
-geth --networkid=1719 --port=3210$node_id --nodiscover\
+if [ -n "$4" ]; then
+  opt="$opt --dmckipcdir=$4"
+fi
+
+datadir=$ETH_PATH"node"$node_id
+
+echo "Logging directory : $log_dir"
+echo "Option : $opt"
+echo "Data dir : $datadir"
+
+set -o xtrace
+geth $opt --networkid=1719 --port=3210$node_id --nodiscover\
     --verbosity=6 --vmodule="p2p=2"\
- --datadir=$ETH_PATH/node$node_id --identity="node$node1"  --rpc --rpcport=990$node_id #2> $ETH_PATH/node$node_id/$curdate.log
+ --datadir=$datadir --identity="node$node_id"  --rpc --rpcport=990$node_id 2> "$log_dir/$curdate.log"
 
 # tailf $ETH_PATH/node$node_id/$curdate.log
-
-
