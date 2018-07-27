256ca387d722ffa23aa9d9d4b695886e0fc612dc
Ardhi Putra Pratama H <me@ardhipoetra.web.id>
2018-07-27 09:29:13 +0700

    Add convenience scripts

---
+++ _scripts/attach_command.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+geth --exec "$2" attach ipc:$ETH_PATH/node$1/geth.ipc
---
+++ _scripts/clean_state.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+geth --datadir /home/ardhipoetra/git/eth/gik/storage//node0/ removedb
+geth --datadir /home/ardhipoetra/git/eth/gik/storage//node1/ removedb
+geth --datadir /home/ardhipoetra/git/eth/gik/storage//node2/ removedb
+
+# geth --datadir /home/ardhipoetra/git/eth/gik/storage/node0/ init /home/ardhipoetra/git/eth/gik/storage/head/genesis.json
+# geth --datadir /home/ardhipoetra/git/eth/gik/storage/node1/ init /home/ardhipoetra/git/eth/gik/storage/head/genesis.json
