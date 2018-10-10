bfb9e032ed80d6c2a51334123083d5b36563418c
Ardhi Putra Pratama H <me@ardhipoetra.web.id>
2018-10-11 02:16:48 +0700

    modifying script to just return pid (for DMCK) note : revert to prev version to independent runs

--- _scripts/run_node.sh
+++ _scripts/run_node.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # how to run : run_node <node_id> <logging directory> <[mine/nomine]> <DMCK ipc directory>
-echo "Running the run_node.sh"
+#echo "Running the run_node.sh"
 curdate=`date +"%Y%m%dT%H%M"`
 
 node_id=$1
@@ -11,7 +11,7 @@ opt=" --selfId=$node_id"
 
 if [ -n "$2" ]; then
   if [ "$2" == "mine" ]; then
-    opt="$opt --mine --minerthreads=1"
+    opt="$opt --mine --minerthreads=2"
   else
     log_dir=$2
   fi
@@ -19,7 +19,7 @@ fi
 
 if [ -n "$3" ]; then
   if [ "$3" == "mine" ]; then
-    opt="$opt --mine --minerthreads=1"
+    opt="$opt --mine --minerthreads=2"
   fi
 fi
 
@@ -29,13 +29,13 @@ fi
 
 datadir=$ETH_PATH"node"$node_id
 
-echo "Logging directory : $log_dir"
-echo "Option : $opt"
-echo "Data dir : $datadir"
-
-set -o xtrace
+#echo "Logging directory : $log_dir"
+#echo "Option : $opt"
+#echo "Data dir : $datadir"
+#
+#set -o xtrace
 geth $opt --networkid=1719 --port=3210$node_id --nodiscover\
-    --verbosity=6 --vmodule="p2p=2"\
- --datadir=$datadir --identity="node$node_id"  --rpc --rpcport=990$node_id 2> "$log_dir/$curdate.log"
-
+    --verbosity=4 --vmodule="p2p/*=0,core/state/*=0"\
+ --datadir=$datadir --identity="node$node_id"  --rpc --rpcport=990$node_id > "$log_dir/$curdate.log" 2>&1  &
+echo $!
 # tailf $ETH_PATH/node$node_id/$curdate.log
